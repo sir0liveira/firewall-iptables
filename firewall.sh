a6fc70d8ae82373a3c26ff8a0c6b8b631a02498d
sir0liveira <contato@sirlinux.com.br>
2020-08-04 08:07:28 -0300

    Acrescentei portas na tabela NAT

--- firewall.sh
+++ firewall.sh
@@ -3,41 +3,45 @@
 case $1 in
 
 	start)
+		
 		iptables -F
 		iptables -F -t nat
 		iptables -F -t mangle
 		iptables -P INPUT DROP
 		iptables -P OUTPUT ACCEPT
 		iptables -P FORWARD DROP
-        	iptables -Z
+        iptables -Z
 
 
 		#Entrada no Firewall
-        	iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-		iptables -A INPUT -p tcp --syn --dport 22 -s 10.10.1.0/24 -j ACCEPT
+        iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+		
+		#Libera SSH
+		iptables -A INPUT -p tcp --syn --dport 19842 -s 10.10.1.0/24 -j ACCEPT
+		
 		iptables -A INPUT -i enp0s3 -m state --state NEW -p udp --dport 5050 -j ACCEPT
 		iptables -A INPUT -p icmp -j ACCEPT	
 
-		
+
 		#Saida no Firewall
 		iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 		iptables -A OUTPUT -p tcp --syn -m multiport --dports 80,443 -j ACCEPT
-		iptables -A OUTPUT -p udp --dport 53 -d 8.8.8.8 -j ACCEPT
-        	iptables -A OUTPUT -p icmp -j ACCEPT
+		iptables -A OUTPUT -p udp --dport 53 -d 8.8.8.8 --sport 1024:65535 -j ACCEPT
+        iptables -A OUTPUT -p icmp -j ACCEPT
 
 
-        	#Compartilhando Internet
-        	iptables -A POSTROUTING -t nat -o enp0s3 -s 10.10.1.0/24 -j MASQUERADE
+        #Compartilhando Internet
+        iptables -t nat  -A POSTROUTING -o enp0s3 -s 10.10.1.0/24 -j MASQUERADE
 		sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 		sysctl -p
 
         
 		#Entrada e Saida da Rede Interna
-        	iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-		iptables -A FORWARD -p tcp --syn -m multiport --dports 22,80,443, 3306 -s 10.10.1.0/24 -j ACCEPT
+        iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+		iptables -A FORWARD -p tcp --syn -m multiport --dports 80,443,465,587,993,995,3050,3306,5222,7777,19842 -s 10.10.1.0/24 -j ACCEPT
 		iptables -A FORWARD -p udp --dport 53 -s 10.10.1.0/24 -j ACCEPT	
 		iptables -A FORWARD -i tap0 -j ACCEPT
-		iptables -A FORWARD -p icmp -j ACCEPT
+		iptables -A FORWARD -p icmp -s 10.10.1.0/24 -j ACCEPT
 
 		
 		#Nat para o server VPN
@@ -45,24 +49,26 @@ case $1 in
 	
 	;;
 	stop)
+		
 		iptables -F
 		iptables -F -t nat
 		iptables -F -t mangle
 		iptables -P INPUT ACCEPT
 		iptables -P OUTPUT ACCEPT
 		iptables -P FORWARD ACCEPT
-        	iptables -Z
+        iptables -Z
 
 		
 		#Desabilitar Compartilhamento de Internet
 		sed -i 's/net.ipv4.ip_forward=1/#net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 		sysctl -p
-
+	
 	;;
 	restart)
-
+		
 		$0 stop
 		$0 start
-
-    ;;
+    
+	;;
+	
 esac
